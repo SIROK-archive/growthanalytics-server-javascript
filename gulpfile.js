cb060781e6d7809ea56f90433c2769a1545b7e02
onda_koichi <onda_koichi@cyberagent.co.jp>
2015-06-01 22:16:24 +0900

    setting file

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/* jshint node:true */
+/* global -$ */
+'use strict';
+
+// Load plugins
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+var $           = require('gulp-load-plugins')();
+
+gulp.task('typescript', function () {
+    gulp.src('src/main.ts')
+        .pipe($.tsc({
+            module: 'amd',
+            comment: true,
+            keepTree: false,
+            out: 'analytics.segment.js'
+        }))
+        .pipe(gulp.dest('target/'))
+        .pipe($.notify({message: 'Typescript compile done.'}));
+});
+
+gulp.task('uglify', function () {
+	var path = 'target/';
+    gulp.src(path + 'analytics.segment.js').pipe($.uglifyjs('analytics.segment.min.js')).pipe(gulp.dest(path));
+});
+
+gulp.task('default', function() {
+	runSequence(
+//		'clear',
+		'typescript',
+		'uglify'
+  );
+	return gulp.src('').pipe($.notify({message: 'Task is done.'}));
+});
+
